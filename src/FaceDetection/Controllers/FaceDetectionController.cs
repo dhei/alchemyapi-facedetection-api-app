d3df34ac4f1b346664e3fd16d94133a1f0bcdafa
Wade Wegner <wade.wegner@gmail.com>
2015-05-03 09:41:24 -0600

    Updated code to include simple Get

--- src/FaceDetection/Controllers/FaceDetectionController.cs
+++ src/FaceDetection/Controllers/FaceDetectionController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using FaceDetection.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -13,10 +16,14 @@ namespace FaceDetection.Controllers
 {
     public class FaceDetectionController : ApiController
     {
-        private const string _alchemyApiKey = "";
+        private const string _alchemyApiKey = "YourAPIKey";
         private const string _url = "http://access.alchemyapi.com/calls/image/ImageGetRankedImageFaceTags";
 
-        public async Task<JToken> PostFormData()
+        /// <summary>
+        /// Posts to the Alchemy API and looks for faces.
+        /// </summary>
+        /// <returns>FaceDetectionResult</returns>
+        public async Task<FaceDetectionResult> PostFormData()
         {
             if (!Request.Content.IsMimeMultipartContent("form-data"))
             {
@@ -38,13 +45,48 @@ namespace FaceDetection.Controllers
                 var responseMessage =
                     await client.PostAsync(alchemyUrl, new StreamContent(fileStream)).ConfigureAwait(false);
 
-                if (!responseMessage.IsSuccessStatusCode) return "error";
+                if (!responseMessage.IsSuccessStatusCode) return null;
 
                 var response = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-                var r = JsonConvert.DeserializeObject<dynamic>(response);
-                return JObject.Parse(r.ToString());
+                var r = JsonConvert.DeserializeObject<FaceDetectionResult>(response);
+                return r;
             }
+        }
+
+        /// <summary>
+        /// Useful for a simple demonstration of the results w/o having to submit a post.
+        /// </summary>
+        /// <returns>FaceDetectionResult</returns>
+        public async Task<FaceDetectionResult> Get()
+        {
+            var httpClient = new HttpClient();
+            using (var content = new MultipartFormDataContent("BOUNDARY"))
+            {
+                var root = HttpContext.Current.Server.MapPath("~/bin");
+                string path = root + "\\wade.jpg";
+                var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                content.Add(new StreamContent(fileStream), "test", "wade.jpg");
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("http://localhost:21346/api/facedetection"),
+                    Content = content
+                };
+
+                var responseMessage = await httpClient.SendAsync(request).ConfigureAwait(false);
+                var response = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var envelope = JsonConvert.DeserializeObject<FaceDetectionResult>(response);
+                    return envelope;
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ src/FaceDetection/Models/FaceDetectionResult.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceDetection.Models
+{
+    public class Age
+    {
+        public string ageRange { get; set; }
+        public string score { get; set; }
+    }
+
+    public class Gender
+    {
+        public string gender { get; set; }
+        public string score { get; set; }
+    }
+
+    public class Disambiguated
+    {
+        public string dbpedia { get; set; }
+        public string freebase { get; set; }
+        public string name { get; set; }
+        public List<string> subType { get; set; }
+        public string website { get; set; }
+        public string yago { get; set; }
+    }
+
+    public class KnowledgeGraph
+    {
+        public string typeHierarchy { get; set; }
+    }
+
+    public class Identity
+    {
+        public Disambiguated disambiguated { get; set; }
+        public KnowledgeGraph knowledgeGraph { get; set; }
+        public string name { get; set; }
+        public string score { get; set; }
+    }
+
+    public class ImageFace
+    {
+        public Age age { get; set; }
+        public Gender gender { get; set; }
+        public string height { get; set; }
+        public Identity identity { get; set; }
+        public string positionX { get; set; }
+        public string positionY { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FaceDetectionResult
+    {
+        public string status { get; set; }
+        public string usage { get; set; }
+        public string totalTransactions { get; set; }
+        public List<ImageFace> imageFaces { get; set; }
+    }
+}
